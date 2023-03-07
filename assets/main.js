3afd60eab91d11cf3ec727c061b01b9c4c80ce7b
goelv641 <112566005+goelv641@users.noreply.github.com>
2023-03-07 17:12:41 -0500

    pop up still not working

--- assets/main.js
+++ assets/main.js
@@ -7,21 +7,32 @@ window.arenaCallback = () => {
 	console.log(clickablelink)
 	clickablelink.forEach(element => {
 		element.addEventListener("click",function(){
+			let fig=element.querySelector("figure")
+			console.log(fig.className)
 			let title=element.querySelector(".title")
 			let description=element.querySelector(".description")
 			let linkurl=element.querySelector(".link")
+			let image=element.querySelector(".image-display")
+			if(fig.className=="video-embed-block"){
+				document.querySelector("#popup-video").src = linkurl.href;
+			}
 			console.log(element)
-			openPopUp(title.innerHTML,description.innerHTML,linkurl.href)
+			openPopUp(title.innerHTML,description.innerHTML,linkurl.href,image.srcset)
 			
 		})
 	});
 }
 
-function openPopUp(popuptitle,popupdescription,url) {
+function openPopUp(popuptitle,popupdescription,url,srcset) {
 	document.querySelector(".popup").style.display = "block";
 	document.querySelector("#popup-title").innerHTML = popuptitle;
 	document.querySelector("#popup-description").innerHTML = popupdescription;
 	document.querySelector("#popup-title").href = url;
+	document.querySelector("#popup-image").srcset = srcset;
    }
 
+   document.querySelector("#close").addEventListener("click",function(){
+	document.querySelector(".popup").style.display = "none";
+   })
 
+   
